b52ff5d2e125530cc18bdce39e83ebc79227f66c
ShachiMishra <99087915+ShachiMishra@users.noreply.github.com>
2022-02-13 14:31:27 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 13 14:26:59 2022
+
+@author: Shachi Mishra
+"""
+
+PI = 3.14
+r = float(input("Enter the radius of a circle:"))
+area = PI * r * r
+print("Area of a circle = %.2f" %area)
