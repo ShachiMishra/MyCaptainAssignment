e3b0f02fe5030de2a3d4f9499a7195d463ca2709
ShachiMishra <99087915+ShachiMishra@users.noreply.github.com>
2022-02-28 15:31:02 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 28 15:21:35 2022
+
+@author: Shachi Mishra
+"""
+
+# Python program to print positive Numbers in a List
+  
+# list of numbers
+list1 = [12,-7,5,64,-14]
+  
+# iterating each number in list
+for num in list1:
+      
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
+       
+       
+       
+       
+# list of numbers
+list2 = [12,14,-95,3] 
+
+# iterating each number in list
+for num in list2:
+      
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
+  
